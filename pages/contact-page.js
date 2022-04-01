750a64305b073c6e5868e408f0986022ed343925
hikaby <hikaby2021@gmail.com>
2022-04-01 11:07:16 +0900

    change to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,31 +1,31 @@
-import Layout from '../components/Layout';
-import Image from 'next/image'
+import Layout from "../components/Layout";
+import Image from "next/image";
 
-const Contact = () => {
+export default function Contact() {
   return (
-    <Layout title='Contact'>
-      <div className='bg-white text-center shadow-xl p-8 w-80 rounded'>
-        <div className='mt-4'>
-          <p className='font-bold'>Contact info</p>
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contact info</p>
         </div>
-        <div className=' flex justify-center mt-4'>
+        <div className=" flex justify-center mt-4">
           <Image
-            className='rounded-full'
-            src='/avatar.jpg'
+            className="rounded-full"
+            src="/avatar.jpg"
             width={60}
             height={60}
-            alt='avatar'
+            alt="avatar"
           />
         </div>
-        <div className='mt-4'>
-          <p className='font-bold'>Address</p>
-          <p className='text-xs mt-2 text-gray-600'>city A</p>
-          <p className='font-bold mt-3'>E-mail</p>
-          <p className=' text-xs mt-2 text-gray-600'>abc@email.com</p>
-          <p className='font-bold mt-3'>Phone</p>
-          <p className=' text-xs mt-2 text-gray-600'>000-123-456</p>
+        <div className="mt-4">
+          <p className="font-bold">Address</p>
+          <p className="text-xs mt-2 text-gray-600">city A</p>
+          <p className="font-bold mt-3">E-mail</p>
+          <p className=" text-xs mt-2 text-gray-600">abc@email.com</p>
+          <p className="font-bold mt-3">Phone</p>
+          <p className=" text-xs mt-2 text-gray-600">000-123-456</p>
         </div>
-        <div className='mt-6 flex justify-around'>
+        <div className="mt-6 flex justify-around">
           <div>
             <a
               href="https://nerdcave.com/tailwind-cheat-sheet"
@@ -78,7 +78,5 @@ const Contact = () => {
         </div>
       </div>
     </Layout>
-  )
+  );
 }
-
-export default Contact;
